263fb297983c5abea5363e94de7242627c4931b8
Alex <avlonitis@msn.com>
2021-11-21 11:18:03 +0000

    Add model methods specs

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Room, type: :model do
   describe 'associations' do
     it { should have_many(:messages).dependent(:destroy) }
   end
+
+  describe '#last_weeks_messages_count' do
+    let(:user) { build(:user) }
+    let(:room) { build(:room) }
+    let!(:message1) { create(:message, created_at: 8.days.ago, room: room, user: user) }
+    let!(:message2) { create(:message, created_at: 1.week.ago, room: room, user: user) }
+    let!(:message3) { create(:message, room: room, user: user) }
+
+    context 'when it does not have a date param' do
+      it 'returns all messages from last week until now' do
+        expect(room.last_weeks_messages_count).to eq 2
+      end
+    end
+
+    context 'when it has a date param' do
+      it 'returns all messages from last week until the given date' do
+        expect(room.last_weeks_messages_count(1.day.ago)).to eq 1
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_many(:messages).dependent(:destroy) }
+
+  describe '#last_message_date' do
+    let(:user) { build(:user) }
+    let(:room) { build(:room) }
+    let(:date) { '01-11-2021'.to_date }
+    let(:last_date) { '11-11-2021'.to_date }
+    let!(:message1) { create(:message, created_at: date, room: room, user: user) }
+    let!(:message2) { create(:message, created_at: last_date, room: room, user: user) }
+
+    it 'returns the date of the last message' do
+      expect(user.last_message_date).to eq last_date
+    end
+  end
 end
